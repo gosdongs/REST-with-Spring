a448539f64c209e3d5396d501576ffad67a50392
Tommy Son <tommy.m.son@gmail.com>
2024-05-28 00:15:51 -0700

    Add EmployeeRepository interface

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -16,9 +16,10 @@ class Employee {
     private String role;
 
     public Employee() {
+
     }
 
-    public Employee(String name, String role) {
+    Employee(String name, String role) {
         this.name = name;
         this.role = role;
     }
